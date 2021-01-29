6db9baf81ad9f5718d16fb24d6e9e5af295a94b4
Gustavo Moro <gustavolego41@gmail.com>
2021-01-29 19:09:12 -0300

    acrescentei o resultado

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace testedetrasbalho
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("TESTANDODDDDD");
+
+            int b = 1;
+            var a = 20;
+            var resultado = a + b;
         }
     }
 }
