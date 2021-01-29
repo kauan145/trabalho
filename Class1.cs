cf77049a32245869e57530d8debbf434eeab93e9
Gustavo Moro <78118591+Gustavolego41@users.noreply.github.com>
2021-01-29 19:24:23 -0300

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -13,10 +13,7 @@ namespace testedetrasbalho
 
         public static class Opa
         {
-            //testa
-            //dsadsadsadsadsa
-            ///dsadsad
-            ///dsadsad
+
         }
     }
 }
